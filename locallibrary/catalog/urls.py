e8ce0c76fc7085945f6261fecab660fadbf2f3f8
Ksenia277 <shahhovakeenis.2005@mail.ru>
2023-11-14 21:21:46 +0700

    Обновление формы с помощью представления формы и функции

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -14,3 +14,6 @@ urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
 
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
