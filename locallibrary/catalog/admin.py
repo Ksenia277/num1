57b267cd7fdfa81b301589396c0cd3e744cb04a5
Ksenia277 <shahhovakeenis.2005@mail.ru>
2023-11-14 18:10:17 +0700

    Создание странцы списка книг и сессии

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -4,8 +4,6 @@ from .models import Author, Genre, Book, BookInstance
 # admin.site.register(Book)
 # admin.site.register(Author)
 admin.site.register(Genre)
-
-
 # admin.site.register(BookInstance)
 
 class AuthorAdmin(admin.ModelAdmin):
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render
 from .models import *
+from django.views import generic
 
 
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
 
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.filter(
+    status__exact='a').count()
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     return render(
-        request,
-        'index.html',
-        context={'num_books': num_books, 'num_instances': num_instances,
-                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
-    )
+    request,
+    'index.html',
+    context={'num_books': num_books, 'num_instances': num_instances,
+    'num_instances_available': num_instances_available, 'num_authors': num_authors,
+    'num_visits': num_visits},
+)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
